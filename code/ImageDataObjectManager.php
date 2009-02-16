feba59a6621e942898bd874f4b943f3bfc9227c4
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-02-16 05:42:04 +0000

    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@21 f1759cf2-80a2-421f-8334-f6f125b70621

---
+++ code/ImageDataObjectManager.php
@@ -0,0 +1,109 @@
+<?php
+
+class ImageDataObjectManager extends FileDataObjectManager
+{
+	protected static $sliderWidth = 150;
+	protected static $minImageSize = 25;
+	protected static $maxImageSize = 300;
+
+
+	public $view = "grid";
+	protected $limitFileTypes = array ('jpg');
+	public $template = "ImageDataObjectManager";
+	public $itemClass = "ImageDataObjectManager_Item";
+	public $popupClass = "ImageDataObjectManager_Popup";
+	
+	public $imageSize = 100;
+
+	public function __construct($controller, $name, $sourceClass, $fileFieldName, $fieldList = null, $detailFormFields = null, $sourceFilter = "", $sourceSort = "", $sourceJoin = "") 
+	{
+		parent::__construct($controller, $name, $sourceClass, $fileFieldName, $fieldList, $detailFormFields, $sourceFilter, $sourceSort, $sourceJoin); 
+		Requirements::css('dataobject_manager/css/ui/ui.all.css');
+		Requirements::javascript('dataobject_manager/javascript/imagedataobject_manager.js');
+		Requirements::css('dataobject_manager/css/imagedataobject_manager.css');
+
+		if(isset($_REQUEST['ctf'][$this->Name()])) {		
+				$this->imageSize = $_REQUEST['ctf'][$this->Name()]['imagesize'];
+		}
+
+	
+		$this->setAllowedFileTypes($this->limitFileTypes);
+	}
+
+	function handleItem($request) {
+		return new ImageDataObjectManager_ItemRequest($this, $request->param('ID'));
+	}
+
+	protected function getQueryString($params = array())
+	{ 
+		$imagesize = isset($params['imagesize'])? $params['imagesize'] : $this->imageSize;
+		return parent::getQueryString($params)."&ctf[{$this->Name()}][imagesize]={$imagesize}";
+	}
+	
+	public function SliderPercentage()
+	{
+		return ($this->imageSize - self::$minImageSize) / ((self::$maxImageSize - self::$minImageSize) / 100);
+	}
+	
+	public function SliderPosition()
+	{
+		return floor(($this->SliderPercentage()/100) * self::$sliderWidth) - (.17 * $this->SliderPercentage()); // handle is 16px wide
+	}
+		
+
+}
+
+class ImageDataObjectManager_Item extends FileDataObjectManager_Item 
+{
+
+	function __construct(DataObject $item, ComplexTableField $parent, $start)
+	{
+		parent::__construct($item, $parent, $start);
+	}
+
+	public function FileIcon()
+	{
+		$file = $this->obj($this->parent->fileFieldName);
+		if($this->parent->imageSize <= 50) $size = 50;
+		elseif($this->parent->imageSize <= 100) $size = 100;
+		elseif($this->parent->imageSize <= 200) $size = 200;
+		else $size = 300;
+		return ($file instanceof Image) ? $file->CroppedImage($size, $size)->URL : $file->Icon();
+	}
+	
+	public function ImageSize()
+	{
+		return $this->parent->imageSize;
+	}
+
+}
+
+class ImageDataObjectManager_Popup extends FileDataObjectManager_Popup
+{
+	function __construct($controller, $name, $fields, $validator, $readonly, $dataObject) 
+	{
+			parent::__construct($controller, $name, $fields, $validator, $readonly, $dataObject);
+			Requirements::css('dataobject_manager/css/imagedataobject_manager.css');
+	}
+
+}
+
+class ImageDataObjectManager_ItemRequest extends DataObjectManager_ItemRequest 
+{
+	function __construct($ctf, $itemID) 
+	{
+		parent::__construct($ctf, $itemID);
+	}
+	
+	function DetailForm($childID = null)
+	{
+		$fileField = $this->ctf->fileFieldName;
+		$imgObj = $this->dataObj()->$fileField();
+		$form = parent::DetailForm($childID);
+		$form->Fields()->insertBefore(new LiteralField("preview", "<div class='preview-image'><img src='".$imgObj->SetHeight(200)->URL."' alt='' /></div>"), $form->Fields()->First()->Name());
+		return $form;
+	}
+
+}
+
+?>
