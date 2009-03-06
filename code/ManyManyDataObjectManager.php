a8cd6c39626d79204b7261bdcea88f64cdf553ae
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-03-06 19:09:34 +0000

    ManyManyDataObjectManager!
    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@56 f1759cf2-80a2-421f-8334-f6f125b70621

---
+++ code/ManyManyDataObjectManager.php
@@ -0,0 +1,103 @@
+<?php
+
+class ManyManyDataObjectManager extends HasManyDataObjectManager
+{
+	private $manyManyParentClass;
+	
+	public $itemClass = 'ManyManyDataObjectManager_Item';
+		
+	function __construct($controller, $name, $sourceClass, $fieldList, $detailFormFields = null, $sourceFilter = "", $sourceSort = "", $sourceJoin = "") {
+
+		parent::__construct($controller, $name, $sourceClass, $fieldList, $detailFormFields, $sourceFilter, $sourceSort, $sourceJoin);
+		
+		$classes = array_reverse(ClassInfo::ancestry($this->controllerClass()));
+		foreach($classes as $class) {
+			$singleton = singleton($class);
+			$manyManyRelations = $singleton->uninherited('many_many', true);
+			if(isset($manyManyRelations) && array_key_exists($this->name, $manyManyRelations)) {
+				$this->manyManyParentClass = $class;
+				$manyManyTable = $class . '_' . $this->name;
+				break;
+			}
+			$belongsManyManyRelations = $singleton->uninherited( 'belongs_many_many', true );
+			 if( isset( $belongsManyManyRelations ) && array_key_exists( $this->name, $belongsManyManyRelations ) ) {
+				$this->manyManyParentClass = $class;
+				$manyManyTable = $belongsManyManyRelations[$this->name] . '_' . $this->name;
+				break;
+			}
+		}
+		$tableClasses = ClassInfo::dataClassesFor($this->sourceClass);
+		$source = array_shift($tableClasses);
+		$sourceField = $this->sourceClass;
+		if($this->manyManyParentClass == $sourceField)
+			$sourceField = 'Child';
+		$parentID = $this->controller->ID;
+		
+		$this->sourceJoin .= " LEFT JOIN `$manyManyTable` ON (`$source`.`ID` = `{$sourceField}ID` AND `{$this->manyManyParentClass}ID` = '$parentID')";
+		
+		$this->joinField = 'Checked';
+	}
+		
+	function getQuery($limitClause = null) {
+		if($this->customQuery) {
+			$query = $this->customQuery;
+			$query->select[] = "{$this->sourceClass}.ID AS ID";
+			$query->select[] = "{$this->sourceClass}.ClassName AS ClassName";
+			$query->select[] = "{$this->sourceClass}.ClassName AS RecordClassName";
+		}
+		else {
+			$query = singleton($this->sourceClass)->extendedSQL($this->sourceFilter, $this->sourceSort, $limitClause, $this->sourceJoin);
+			
+			// Add more selected fields if they are from joined table.
+
+			$SNG = singleton($this->sourceClass);
+			foreach($this->FieldList() as $k => $title) {
+				if(! $SNG->hasField($k) && ! $SNG->hasMethod('get' . $k))
+					$query->select[] = $k;
+			}
+			$parent = $this->controllerClass();
+			$query->select[] = "IF(`{$this->manyManyParentClass}ID` IS NULL, '0', '1') AS Checked";
+		}
+		return clone $query;
+	}
+		
+	function getParentIdName($parentClass, $childClass) {
+		return $this->getParentIdNameRelation($parentClass, $childClass, 'many_many');
+	}
+			
+	function ExtraData() {
+		$items = array();
+		foreach($this->unpagedSourceItems as $item) {
+			if($item->{$this->joinField})
+				$items[] = $item->ID;
+		}
+		$list = implode(',', $items);
+		$value = ",";
+		$value .= !empty($list) ? $list."," : "";
+		$inputId = $this->id() . '_' . $this->htmlListEndName;
+		return <<<HTML
+		<input id="$inputId" name="{$this->name}[{$this->htmlListField}]" type="hidden" value="$value"/>
+HTML;
+	}
+
+
+}
+
+class ManyManyDataObjectManager_Item extends DataObjectManager_Item {
+	
+	function MarkingCheckbox() {
+		$name = $this->parent->Name() . '[]';
+		
+		if($this->parent->IsReadOnly)
+			return "<input class=\"checkbox\" type=\"checkbox\" name=\"$name\" value=\"{$this->item->ID}\" disabled=\"disabled\"/>";
+		else if($this->item->{$this->parent->joinField})
+			return "<input class=\"checkbox\" type=\"checkbox\" name=\"$name\" value=\"{$this->item->ID}\" checked=\"checked\"/>";
+		else
+			return "<input class=\"checkbox\" type=\"checkbox\" name=\"$name\" value=\"{$this->item->ID}\"/>";
+	}
+}
+
+
+
+
+?>
