0b9811f486be9c173b3ef6e0be4e6ecc4f5f7660
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-03-06 22:09:53 +0000

    configuration
    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@61 f1759cf2-80a2-421f-8334-f6f125b70621

--- code/SimpleHTMLEditorField.php
+++ code/SimpleHTMLEditorField.php
@@ -2,11 +2,56 @@
 
 class SimpleHTMLEditorField extends TextareaField
 {
-	
-	function __construct($name, $title = null, $rows = 5, $cols = 55
-	, $value = "", $form = null) {
+	protected $css;
+	protected $controls = array (
+		'insertOrderedList' 	=>	true,
+		'insertUnorderedList'	=> 	true,
+		'justifyLeft' 			=>	true,
+		'justifyRight' 			=> 	true,
+		'justifyCenter'			=> 	true,
+		'justifyFull' 			=> 	false,
+		'cut' 					=> 	false,
+		'copy' 					=>	false,
+		'paste' 				=> 	false,
+		'increaseFontSize' 		=> 	true,
+		'decreaseFontSize' 		=> 	true
+	);
+
+	function __construct($name, $title = null, $config = array(), $rows = 5, $cols = 55, $value = "", $form = null) {
 		parent::__construct($name, $title, $rows, $cols, $value, $form);
 		$this->extraClasses = array('hidden');
+		if(!empty($config)) {
+			foreach($config as $k => $v) {
+				if($k == "css") $this->css = $v;
+				else if(array_key_exists($k, $this->controls))
+					$this->controls[$k] = $v;
+			}
+		}
+	}
+	
+	private function getCss()
+	{
+		return $this->css ? "css : '{$this->css}'" : "css : ''";
+	}
+	
+	private function getControls()
+	{
+		$controls = "controls : {\n";
+		$first = true;
+		foreach($this->controls as $var => $value) {
+			$controls .= $first ? "" : ",";
+			$controls .= $var . " : ";
+			$controls .= $value ? "{visible : true}" : "{visible : false}";
+			$controls .=  "\n";
+			$first = false;
+		}
+		$controls .= "},\n";
+		return $controls;
+	}
+	
+	private function getConfig()
+	{
+		return $this->getControls().$this->getCss();
 	}
 	
 	public function FieldHolder()
@@ -15,7 +60,9 @@ class SimpleHTMLEditorField extends TextareaField
 		Requirements::css('dataobject_manager/css/jquery.wysiwyg.css');
 		Requirements::customScript("
 			$(function() {
-				$('#{$this->id()}').wysiwyg().parents('.simplehtmleditor').removeClass('hidden');
+				$('#{$this->id()}').wysiwyg({
+					{$this->getConfig()}
+				}).parents('.simplehtmleditor').removeClass('hidden');
 				
 			});
 		");
