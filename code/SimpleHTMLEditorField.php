be0fd770c5fe03c59106dc42159cf5c8218d95d2
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-03-06 18:52:06 +0000

    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@50 f1759cf2-80a2-421f-8334-f6f125b70621

---
+++ code/SimpleHTMLEditorField.php
@@ -0,0 +1,29 @@
+<?php
+
+class SimpleHTMLEditorField extends TextareaField
+{
+	
+	function __construct($name, $title = null, $rows = 5, $cols = 20, $value = "", $form = null) {
+		parent::__construct($name, $title, $rows, $cols, $value, $form);
+		$this->extraClasses = array('hidden');
+	}
+	
+	public function FieldHolder()
+	{
+		Requirements::javascript('dataobject_manager/javascript/jquery.wysiwyg.js');
+		Requirements::css('dataobject_manager/css/jquery.wysiwyg.css');
+		Requirements::customScript("
+			$(function() {
+				$('#{$this->id()}').wysiwyg().parents('.simplehtmleditor').removeClass('hidden');
+				
+			});
+		");
+		return parent::FieldHolder();		
+	}	
+	
+}
+
+
+
+
+?>
