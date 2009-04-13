71701f862c88a32dc96e5b1cd5e94e7cde73cdb1
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-04-13 18:46:48 +0000

    new js to support lack of field holder
    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@116 f1759cf2-80a2-421f-8334-f6f125b70621

--- javascript/filedataobjectmanager_popup.js
+++ javascript/filedataobjectmanager_popup.js
@@ -1,33 +1,35 @@
-$(function() {
-	if($('#ImportFolder select')) {
-		$('#ImportFolder select').change(function() {
-			if($(this).val() == '')  {
-				$('#import-holder').html('');
-				$('#action_saveUploadForm').show();
-			}
-			else {
-				$('#import-holder').load($(this).val(), {},function() {
-					$('#action_saveUploadForm').hide();
-					$('#Form_ImportForm li').click(function(e) {
-						if(e.target.nodeName != "INPUT") {
-							i = $(this).find('input'); 
-							c = i.attr('checked'); 
-							i.attr('checked', !c);
-						}
-						$(this).toggleClass('current');
-						e.stopPropagation();
+(function($) {
+	$(function() {
+		if($('select#ImportFolder')) {
+			$('select#ImportFolder').change(function() {
+				if($(this).val() == '')  {
+					$('#import-holder').html('');
+					$('#action_saveUploadForm').show();
+				}
+				else {
+					$('#import-holder').load($(this).val(), {},function() {
+						$('#action_saveUploadForm').hide();
+						$('#Form_ImportForm li').click(function(e) {
+							if(e.target.nodeName != "INPUT") {
+								i = $(this).find('input'); 
+								c = i.attr('checked'); 
+								i.attr('checked', !c);
+							}
+							$(this).toggleClass('current');
+							e.stopPropagation();
+						});
 					});
-				});
-			}				 
-		}).find('option:contains((0 files))').attr('disabled',true);
-	}
-});
-
-$().ajaxSend(function(r,s){  
- $(".ajax-loader").fadeIn("fast");  
-});  
-   
-$().ajaxStop(function(r,s){  
-  $(".ajax-loader").fadeOut("fast");  
-});  
-
+				}				 
+			}).find('option:contains((0 files))').attr('disabled',true);
+		}
+	});
+	
+	$().ajaxSend(function(r,s){  
+	 $(".ajax-loader").fadeIn("fast");  
+	});  
+	   
+	$().ajaxStop(function(r,s){  
+	  $(".ajax-loader").fadeOut("fast");  
+	});  
+	
+})(jQuery);
