b6e32150b403d56f055c32bfc87486ed47a714ef
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-06-15 17:19:39 +0000

    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@175 f1759cf2-80a2-421f-8334-f6f125b70621

---
+++ lang/es_ES.php
@@ -0,0 +1,43 @@
+<?php
+
+global $lang;
+
+$lang['es_ES']['DataObjectManager']['ADD'] = 'Adicionar';
+$lang['es_ES']['DataObjectManager']['ADDEDNEW'] = 'Adicionados %s nuevos registros';
+$lang['es_ES']['DataObjectManager']['ADDITEM'] = array(
+	'Adicionar %s',
+	PR_MEDIUM,
+	'Adicionar [nombre]'
+);
+$lang['es_ES']['DataObjectManager']['ALLOWEDFILETYPES'] = 'Solo son permitidos archivos de tipo %s';
+$lang['es_ES']['DataObjectManager']['DELETE'] = 'Borrar';
+$lang['es_ES']['DataObjectManager']['DISPLAYING'] = 'Mostrando';
+$lang['es_ES']['DataObjectManager']['DRAGDROP'] = 'Permiter reordenar con arrastrar y soltar';
+$lang['es_ES']['DataObjectManager']['ERROR'] = 'Error';
+$lang['es_ES']['DataObjectManager']['IMPORTFROMFOLDER'] = 'Importar de un directorio existente';
+$lang['es_ES']['DataObjectManager']['NOFILTER'] = 'Sin filtro';
+$lang['es_ES']['DataObjectManager']['NOITEMSFOUND'] = 'Registros no encontrados';
+$lang['es_ES']['DataObjectManager']['OF'] = 'de';
+$lang['es_ES']['DataObjectManager']['PERPAGERESULTS'] = 'Resultados por página';
+$lang['es_ES']['DataObjectManager']['PERPAGESHOW'] = 'Mostrar';
+$lang['es_ES']['DataObjectManager']['REFRESH'] = 'refrescar';
+$lang['es_ES']['DataObjectManager']['SAVE'] = 'Grabar';
+$lang['es_ES']['DataObjectManager']['SAVEANDADD'] = 'Grabar y adicionar %s';
+$lang['es_ES']['DataObjectManager']['SAVED'] = '%s Grabados exitosamente';
+$lang['es_ES']['DataObjectManager']['SEARCH'] = 'Buscar';
+$lang['es_ES']['DataObjectManager']['SELECTFOLDER'] = 'Seleccionar un directorio';
+$lang['es_ES']['DataObjectManager']['SILVERSTRIPEVERSION'] = 'DataObjectManager requiere Silverstripe versión 2.3 o superior.';
+$lang['es_ES']['DataObjectManager']['TO'] = 'a';
+$lang['es_ES']['DataObjectManager']['TOTALNOT100'] = 'El ancho de las columnas debe ser en total 100 y no %s';
+$lang['es_ES']['DataObjectManager']['UPLOADFROMPC'] = 'Subir desde mi equipo';
+$lang['es_ES']['DataObjectManager']['VIEWFIRST'] = 'Ver primero';
+$lang['es_ES']['DataObjectManager']['VIEWLAST'] = 'Ver último';
+$lang['es_ES']['DataObjectManager']['VIEWNEXT'] = 'Ver siguiente';
+$lang['es_ES']['DataObjectManager']['VIEWPREVIOUS'] = 'Ver anterior';
+$lang['es_ES']['DataObjectManager_popup.ss']['NEXT'] = 'Próximo';
+$lang['es_ES']['DataObjectManager_popup.ss']['PREVIOUS'] = 'Anterior';
+$lang['es_ES']['FileDataObjectManager']['GRIDVIEW'] = 'Vista de grilla';
+$lang['es_ES']['FileDataObjectManager']['LISTVIEW'] = 'Vista en lista';
+$lang['es_ES']['FileDataObjectManager']['SWFUPLOAD'] = 'DataObjectManager requiere el modulo SWFUpload.';
+
+?>
