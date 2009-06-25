a87836c37de89caf34d008f33cc3a2ead6bcaea2
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-06-25 17:49:49 +0000

    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@185 f1759cf2-80a2-421f-8334-f6f125b70621

---
+++ code/AssetManagerFolder.php
@@ -0,0 +1,26 @@
+<?php
+
+class AssetManagerFolder extends DataObjectDecorator
+{
+  
+  public function updateCMSFields(Fieldset $fields)
+  {
+    $fields->removeFieldFromTab("Root.Files","Files");
+    $fields->removeFieldFromTab("Root.Files","deletemarked");
+    $fields->removeByName("Upload");
+    $fields->addFieldToTab("Root.Files", $a = new AssetManager(
+      $this->owner,
+      "Files"
+    ));
+    $a->setUploadFolder($this->owner->Filename);
+    $a->setColumnWidths(array(
+      'Title' => 30,
+      'Filename' => 70
+    ));
+    $a->setSourceFilter("Classname != 'Folder' AND ParentID = ".$this->owner->ID);
+    $a->setParentClass("Folder");
+    return $fields;
+  }
+}
+
+?>
