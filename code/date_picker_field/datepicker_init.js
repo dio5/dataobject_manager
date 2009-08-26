6f2cf46fe4f4bfb97209fdaf8354469398060228
unclecheese <unclecheese@f1759cf2-80a2-421f-8334-f6f125b70621>
2009-08-26 16:04:06 +0000

    date picker update
    git-svn-id: http://carlinowebdesign.svn.beanstalkapp.com/modules/trunk/dataobject_manager@242 f1759cf2-80a2-421f-8334-f6f125b70621

--- code/date_picker_field/datepicker_init.js
+++ code/date_picker_field/datepicker_init.js
@@ -1,9 +1,11 @@
-var datepicker_date_format;
+var date_picker_format;
 (function($) {
   $(function() {
-    $.get('DatePickerField_Controller/dateformat/',function(data) {date_format = data});
-    $('.datepicker input').livequery(function() {
-      $(this).datepicker({dateFormat : datepicker_date_format, buttonImage : '/sapphire/images/calendar-icon.gif', buttonImageOnly : true});
+    $.get('DatePickerField_Controller/dateformat/',function(data) {
+      date_picker_format = data;
+      $('.datepicker input').livequery(function() {
+        $(this).datepicker({dateFormat : date_picker_format, buttonImage : '/sapphire/images/calendar-icon.gif', buttonImageOnly : true});
+      });
     });
   });
 })(jQuery)
